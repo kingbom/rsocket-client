118db0a669b7e37dd9734948d1b850df94dd8192
jaruwit <jaruwit.su@tdshop.io>
2020-11-09 01:17:50 +0700

    [Add] RSocketRequester connect to rsocker service

---
+++ src/main/java/com/kingbom/rsocketclient/GreetingRequest.java
@@ -0,0 +1,11 @@
+package com.kingbom.rsocketclient;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GreetingRequest {
+    private String name;
+}
--- src/main/java/com/kingbom/rsocketclient/RsocketClientApplication.java
+++ src/main/java/com/kingbom/rsocketclient/RsocketClientApplication.java
@@ -1,13 +1,34 @@
 package com.kingbom.rsocketclient;
 
+import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.rsocket.RSocketRequester;
 
 @SpringBootApplication
 public class RsocketClientApplication {
 
+	@SneakyThrows
 	public static void main(String[] args) {
 		SpringApplication.run(RsocketClientApplication.class, args);
+		System.in.read();
 	}
 
+	@Bean
+	public RSocketRequester rSocketRequester(RSocketRequester.Builder builder) {
+		return builder.connectTcp("localhost", 8888).block();
+	}
+
+	@Bean
+	public ApplicationListener<ApplicationReadyEvent> client(RSocketRequester client) {
+		return arguments -> {
+			var greetingResponse = client.route("greetings").data(new GreetingRequest("Kingbom")).retrieveFlux(GreetingResponse.class);
+			greetingResponse.subscribe(System.out::println);
+		};
+	}
 }
+
+
